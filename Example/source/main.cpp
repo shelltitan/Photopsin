79ed224d2d08f15ea446874219857dda5e4ba161
Istvan Szalai <17dszi@gmail.com>
2026-06-21 20:58:54 +0100

    Update main.cpp

--- Example/source/main.cpp
+++ Example/source/main.cpp
@@ -1,15 +1,29 @@
 #include <SDL3/SDL.h>
 #include <spdlog/spdlog.h>
+#include <atomic>
 
-SDL_Window *m_window{nullptr};
+static std::atomic_flag is_running{};
 
-int main() {
+static SDL_Window *window;
+static SDL_Renderer *renderer;
+
+auto main() -> int {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != true) {
-        spdlog::error("Unable to initialise SDL! SDL error: {}", SDL_GetError());
+        //pdlog::info("Unable to initialise SDL! SDL error: {}", SDL_GetError());
         return -1;
     }
 
+    if (window = SDL_CreateWindow("Software renderer app", 800,
+    600, SDL_WINDOW_BORDERLESS | SDL_WINDOW_INPUT_FOCUS | SDL_WINDOW_MOUSE_FOCUS |
+SDL_WINDOW_HIGH_PIXEL_DENSITY);
+        window == nullptr) {
+        //spdlog::info("Window could not be created! SDL error: {}", SDL_GetError());
+        return -1;
+    }
 
+    if (renderer = SDL_CreateRenderer(window, nullptr); renderer == nullptr) {
+        return -1;
+    }
 
     return 0;
 }
