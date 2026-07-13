6e29802e174882075c870323ac4f7cc736ba590f
Istvan Szalai <17dszi@gmail.com>
2026-07-13 01:31:31 +0100

    Little bit of maths

---
+++ include/Maths.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+struct Vector2f {
+    float x;
+    float y;
+};
+
+struct Vector3f {
+    float x;
+    float y;
+    float z;
+};
+
+struct Vector4f {
+    float x;
+    float y;
+    float z;
+    float w;
+};
+
+struct Vector4i {
+    int x;
+    int y;
+    int z;
+    int w;
+};
+
+struct Vector4u {
+    unsigned int x;
+    unsigned int y;
+    unsigned int z;
+    unsigned int w;
+};
